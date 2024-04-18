f92a8008611cc29b621cf5e4cc1dc356e8500b9b
IdentityZero <lovelycadiente1@gmail.com>
2024-04-18 14:09:09 +0800

    User Registration

--- SPMS/Users/forms.py
+++ SPMS/Users/forms.py
@@ -2,9 +2,18 @@ from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
 
+from .models import UsersProfile
+
+
 class UserRegistrationForm(UserCreationForm):
     email = forms.EmailField()
 
     class Meta:
         model = User
         fields = ['username', 'email', 'first_name' ,'last_name', 'password1', 'password2']
+
+
+class UserProfileForm(forms.ModelForm):
+    class Meta:
+        model = UsersProfile
+        fields = ['user_date_of_birth', 'user_sex', 'user_contact_number', 'user_profile_picture']
